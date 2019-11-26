ab4c6232654af8b1668782cfcc90dd3af890b734
Adrian Vladu <avladu@cloudbasesolutions.com>
2019-11-26 20:21:51 +0200

    possbile fix for libssl
    Change-Id: Iacc095067487c03f9b7975b1c2d115aa462683f6

--- cloudbaseinit/utils/crypt.py
+++ cloudbaseinit/utils/crypt.py
@@ -26,7 +26,10 @@ if sys.platform == "win32":
     else:
         clib = ctypes.cdll.ucrtbase
 
-    openssl = ctypes.cdll.libeay32
+    try:
+        openssl = ctypes.cdll.libeay32
+    except Exception:
+        openssl = ctypes.cdll.libcrypto
 else:
     clib = ctypes.CDLL(clib_path)
     openssl_lib_path = ctypes.util.find_library("ssl")
