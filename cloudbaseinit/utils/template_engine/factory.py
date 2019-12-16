ac1d787669830c72bb8038d0e0dfedfd27bfaf89
Adrian Vladu <avladu@cloudbasesolutions.com>
2019-12-16 13:26:45 +0200

    Add support for jinja as userdata template engine
    If the userdata starts with the text '## template:jinja',
    Jinja2 templating engine will be used to render that
    userdata.
    
    This feature leverages the get_instance_data metadata
    service implementation and tries to be as close as possible
    with the cloud-init's implementation.
    
    Change-Id: I79cb0066f7d65ae27867ac01b443e9be432b4dc3

---
+++ cloudbaseinit/utils/template_engine/factory.py
@@ -0,0 +1,44 @@
+# Copyright 2019 Cloudbase Solutions Srl
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import re
+
+from oslo_log import log as oslo_logging
+
+from cloudbaseinit.utils import classloader
+
+TEMPLATE_MATCHER = re.compile(r"##\s*template:(.*)", re.I)
+TEMPLATE_CLASS_PATHS = {
+    "jinja": "cloudbaseinit.utils.template_engine"
+             ".jinja2_template.Jinja2TemplateEngine"
+}
+LOG = oslo_logging.getLogger(__name__)
+
+
+def get_template_manager(userdata):
+    """Returns the template manager depending on the userdata header"""
+
+    if not userdata:
+        return
+
+    template_engine_match = TEMPLATE_MATCHER.match(userdata.decode())
+
+    if template_engine_match:
+        template_engine = template_engine_match.group(1).lower().strip()
+        if TEMPLATE_CLASS_PATHS.get(template_engine, None):
+            cl = classloader.ClassLoader()
+            return cl.load_class(TEMPLATE_CLASS_PATHS[template_engine])()
+        else:
+            raise NotImplementedError("Template engine '%s' is not "
+                                      "supported." % template_engine)
